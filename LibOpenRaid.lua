9b412a6e73e62c6750e8140921144fd3b71a5b97
Grim <bruno.marcetic@gmail.com>
2022-09-09 15:52:42 +0200

    version bump

--- LibOpenRaid.lua
+++ LibOpenRaid.lua
@@ -50,7 +50,7 @@ if (WOW_PROJECT_ID ~= WOW_PROJECT_MAINLINE and not IsDragonflight()) then
 end
 
 local major = "LibOpenRaid-1.0"
-local CONST_LIB_VERSION = 48
+local CONST_LIB_VERSION = 49
 LIB_OPEN_RAID_CAN_LOAD = false
 
 --declae the library within the LibStub
